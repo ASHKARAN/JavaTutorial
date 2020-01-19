fc6c85bc3a6585397a8840311f4f99daea1a624e
ali <ali.ashkaran@gmail.com>
2020-01-19 08:50:07 +0330

    Initial commit

---
+++ src/Navigator.java
@@ -0,0 +1,13 @@
+/**
+ * this class navigate us to the target classes
+ */
+public class Navigator {
+
+    /**
+     * Let's gooooo
+     * @param args
+     */
+    public static void main(String args[]) {
+        System.out.println("Navigator");
+    }
+}
