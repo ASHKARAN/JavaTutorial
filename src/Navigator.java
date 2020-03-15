fdc6a253b40bb5861f34d97b75ad19a11e97eedd
ali <ali.ashkaran@gmail.com>
2020-03-16 02:38:13 +0330

    E032_Constructors

--- src/Navigator.java
+++ src/Navigator.java
@@ -2,6 +2,7 @@ import ir.porteqali.javatutorial.P07_Methods.E028_MethodParameters;
 import ir.porteqali.javatutorial.P08_Classes.E029_Classes;
 import ir.porteqali.javatutorial.P08_Classes.E030_Instance;
 import ir.porteqali.javatutorial.P08_Classes.E031_StaticUsage;
+import ir.porteqali.javatutorial.P08_Classes.E032_Constructors;
 
 /**
  * this class navigate us to the target classes
@@ -15,6 +16,8 @@ public class Navigator {
     public static void main(String args[]) {
         System.out.println("Navigator");
 
-        (new E031_StaticUsage()).Start();
+          new E032_Constructors( ) ;
+          new E032_Constructors("Porteqal" ) ;
+          new E032_Constructors("Porteqal", 2) ;
     }
 }
---
+++ src/ir/porteqali/javatutorial/P08_Classes/E032_Constructors.java
@@ -0,0 +1,24 @@
+package ir.porteqali.javatutorial.P08_Classes;
+
+public class E032_Constructors {
+
+
+    public  E032_Constructors() {
+        System.out.println("E032_Constructors");
+    }
+
+    public  E032_Constructors(String name) {
+        System.out.println("E032_Constructors: " + name);
+    }
+
+    public  E032_Constructors(String name, int age) {
+        System.out.println("E032_Constructors: " + name + " is " + age + "years old");
+    }
+
+
+    public void Start() {
+
+    }
+
+
+}
