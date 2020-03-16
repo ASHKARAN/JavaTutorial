479a5245854271eda646a31e878e976cbffeb73d
ali <ali.ashkaran@gmail.com>
2020-03-17 01:55:21 +0330

    E054_Clone

--- src/ir/porteqali/javatutorial/P10_OOP/E054_Clone.java
+++ src/ir/porteqali/javatutorial/P10_OOP/E054_Clone.java
@@ -3,24 +3,20 @@ package ir.porteqali.javatutorial.P10_OOP;
 public class E054_Clone {
 
     public E054_Clone() {
-        E054_Student student = new E054_Student();
-        student.age = 15 ;
-        student.name = "ali";
+        E054_Student student1 = new E054_Student();
+        student1.name = "ali";
 
-        System.out.println(student .name);
 
+        System.out.println("student1 : " +  student1.name);
 
         E054_Student student2 = null;
         try {
-            student2 = (E054_Student) student.clone();
+            student2 = (E054_Student) student1.clone();
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
-
-        student.name = "reza";
-
-        student2.age = 20 ;
-        student2.name = "mamad";
-        System.out.println(student2.name);
+        student1.name = "gholam";
+        System.out.println("student1 : " +  student1.name);
+        System.out.println("student2 : " +  student2.name);
     }
 }
--- src/ir/porteqali/javatutorial/P10_OOP/E054_Student.java
+++ src/ir/porteqali/javatutorial/P10_OOP/E054_Student.java
@@ -1,9 +1,9 @@
 package ir.porteqali.javatutorial.P10_OOP;
 
 public class E054_Student implements Cloneable {
+    public int age  ;
+    public String name ;
 
-    int age ;
-    String name ;
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
