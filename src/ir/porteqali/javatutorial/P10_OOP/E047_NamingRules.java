2e13dc6fc3358f5307c3d7264727a1aa786070b0
ali <ali.ashkaran@gmail.com>
2020-03-17 00:39:14 +0330

    E047_NamingRules

---
+++ src/ir/porteqali/javatutorial/P10_OOP/E047_NamingRules.java
@@ -0,0 +1,12 @@
+package ir.porteqali.javatutorial.P10_OOP;
+
+public class E047_NamingRules {
+
+    int a = 0;
+    int b ;
+    public E047_NamingRules() {
+
+        String myVeryLargeVariableNameIsSoGood = "wefwef";
+
+    }
+}
