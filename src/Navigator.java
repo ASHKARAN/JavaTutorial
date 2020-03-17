fd4933afc8db88bb15e919d5cb2117ac3319593a
ali <ali.ashkaran@gmail.com>
2020-03-18 00:17:08 +0330

    E062_IF_ELSE_WITHOUT_BLOCK

--- src/Navigator.java
+++ src/Navigator.java
@@ -1,8 +1,4 @@
-import ir.porteqali.javatutorial.P11_SimpleProject.E057_SimpleProject;
-import ir.porteqali.javatutorial.P12_ConditionalStatements.E058_IF;
-import ir.porteqali.javatutorial.P12_ConditionalStatements.E059_IF_ELSE;
-import ir.porteqali.javatutorial.P12_ConditionalStatements.E060_IF_ELSE_IF_ELSE;
-import ir.porteqali.javatutorial.P12_ConditionalStatements.E061_Ternary_Operator;
+import ir.porteqali.javatutorial.P12_ConditionalStatements.*;
 
 /**
  * this class navigate us to the target classes
@@ -15,6 +11,6 @@ public class Navigator {
      */
     public static void main(String args[])  {
         System.out.println("Navigator");
-          new E061_Ternary_Operator() ;
+          new E062_IF_ELSE_WITHOUT_BLOCK() ;
     }
 }
---
+++ src/ir/porteqali/javatutorial/P12_ConditionalStatements/E062_IF_ELSE_WITHOUT_BLOCK.java
@@ -0,0 +1,12 @@
+package ir.porteqali.javatutorial.P12_ConditionalStatements;
+
+public class E062_IF_ELSE_WITHOUT_BLOCK {
+
+    public E062_IF_ELSE_WITHOUT_BLOCK() {
+        int a = 12;
+        int b = 15;
+        if(a>10) System.out.println("inside if");
+        else System.out.println("inside else");
+
+    }
+}
