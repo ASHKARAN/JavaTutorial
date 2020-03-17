a9c0f88555313ee3f6bfdf4b9556b6fb71683c63
ali <ali.ashkaran@gmail.com>
2020-03-18 00:00:49 +0330

    E059_IF_ELSE

--- src/Navigator.java
+++ src/Navigator.java
@@ -1,5 +1,6 @@
 import ir.porteqali.javatutorial.P11_SimpleProject.E057_SimpleProject;
 import ir.porteqali.javatutorial.P12_ConditionalStatements.E058_IF;
+import ir.porteqali.javatutorial.P12_ConditionalStatements.E059_IF_ELSE;
 
 /**
  * this class navigate us to the target classes
@@ -12,6 +13,6 @@ public class Navigator {
      */
     public static void main(String args[])  {
         System.out.println("Navigator");
-          new E058_IF() ;
+          new E059_IF_ELSE() ;
     }
 }
---
+++ src/ir/porteqali/javatutorial/P12_ConditionalStatements/E059_IF_ELSE.java
@@ -0,0 +1,18 @@
+package ir.porteqali.javatutorial.P12_ConditionalStatements;
+
+public class E059_IF_ELSE {
+
+
+    public E059_IF_ELSE() {
+        int a = 10;
+        int b = 15;
+        int c = 12 ;
+
+        if(a>5 && b > 10) {
+            System.out.println("inside if");
+        }
+        else {
+            System.out.println("inside else");
+        }
+    }
+}
