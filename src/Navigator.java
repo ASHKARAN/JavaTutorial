7ebc55cedb58ccd42004cf34d0483df849c55ea5
ali <ali.ashkaran@gmail.com>
2020-03-18 00:46:06 +0330

    E064_Switch_Break

--- src/Navigator.java
+++ src/Navigator.java
@@ -1,4 +1,5 @@
 import ir.porteqali.javatutorial.P12_ConditionalStatements.E063_Switch;
+import ir.porteqali.javatutorial.P12_ConditionalStatements.E064_Switch_Break;
 import ir.porteqali.javatutorial.P13_Arrays.E067_Array_MultiDimensional;
 
 /**
@@ -12,6 +13,6 @@ public class Navigator {
      */
     public static void main(String args[])  {
         System.out.println("Navigator");
-          new E063_Switch() ;
+          new E064_Switch_Break() ;
     }
 }
---
+++ src/ir/porteqali/javatutorial/P12_ConditionalStatements/E064_Switch_Break.java
@@ -0,0 +1,18 @@
+package ir.porteqali.javatutorial.P12_ConditionalStatements;
+
+import ir.porteqali.javatutorial.P10_OOP.E053_Colors;
+
+public class E064_Switch_Break {
+
+    public E064_Switch_Break() {
+        E053_Colors color = E053_Colors.GREEN;
+
+        switch (color) {
+            case BLUE:  System.out.println(E053_Colors.BLUE);   break;
+            case BLACK: System.out.println(E053_Colors.BLACK);  break;
+            case RED:   System.out.println(E053_Colors.RED);    break;
+            case GREEN: System.out.println(E053_Colors.GREEN);  break;
+            case WHITE: System.out.println(E053_Colors.WHITE);  break;
+        }
+    }
+}
