319e914b15fe8a4c22d834479f72707bba038226
ali <ali.ashkaran@gmail.com>
2020-03-18 02:02:31 +0330

    E070_ForLoop

---
+++ src/ir/porteqali/javatutorial/P14_Loops/E070_ForLoop.java
@@ -0,0 +1,14 @@
+package ir.porteqali.javatutorial.P14_Loops;
+
+public class E070_ForLoop {
+
+    public E070_ForLoop() {
+        Integer [] yourArray = {
+                100, 5, 27, 50, 1 , 12,100, 5, 27, 50, 1
+        };
+        for(int i = 0 ; i<yourArray.length ; i++)
+            System.out.println( i + " : " + yourArray[i]);
+
+
+    }
+}
